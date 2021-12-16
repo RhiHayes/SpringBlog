3eda1c107bcef45b25cd65086c26a832a39b11a5
Rhiannon Hayes <rhiannon.nicole.hayes@gmail.com>
2021-12-16 14:52:04 -0600

    Completed Exercise 1 of Controllers

---
+++ src/main/java/com/example/springblog/controllers/HelloController.java
@@ -0,0 +1,33 @@
+package com.example.springblog.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello")
+
+    @ResponseBody
+    public String hello() {
+        return "Hello from Quasar! Happy Holidays!";
+    }
+
+
+    @GetMapping("/hello{name}")
+    @ResponseBody
+
+    public String sayHello(@PathVariable String  name) {
+        return "Hello there " + name + "!";
+    }
+
+
+    @RequestMapping(path = "increment{number}", method = RequestMethod.GET)
+    @ResponseBody
+    
+    public String addOne(@PathVariable int number) {
+        return "Hey, your number plus one is " + (number + 1) + ".";
+    }
+
+    
+}
---
+++ src/main/java/com/example/springblog/controllers/HomeController.java
@@ -0,0 +1,19 @@
+package com.example.springblog.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/landing")
+
+    @ResponseBody
+    public String landingPage() {
+        return "This is the landing page!";
+    }
+
+
+
+}
