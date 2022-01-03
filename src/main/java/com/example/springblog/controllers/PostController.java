16fb122a0172d8eb56b42588dbe23565710c8f4e
Rhiannon Hayes <rhiannon.nicole.hayes@gmail.com>
2022-01-03 14:53:23 -0600

    Added/Completed Exercise 2 for Views

--- src/main/java/com/example/springblog/controllers/PostController.java
+++ src/main/java/com/example/springblog/controllers/PostController.java
@@ -2,25 +2,32 @@ package com.example.springblog.controllers;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
 
 @Controller
 public class PostController {
 
-    @GetMapping("/posts")
 
-    @ResponseBody
-    public String hello() {
-        return "This is the posts index!";
+    @RequestMapping(path = "/posts/index", method = RequestMethod.GET)
+    public String postsIndex(Model model){
+        ArrayList<Post> posts = new ArrayList<>();
+        Post post1 = new Post("title-1", "body-1");
+        Post post2 = new Post("title-2", "body-2");
+        posts.add(post1);
+        posts.add(post2);
+        model.addAttribute("posts", posts);
+        return "/posts/index";
     }
 
 
-    @RequestMapping(path = "posts/{id}", method = RequestMethod.GET)
-
-    @ResponseBody
-
-    public String id(@PathVariable int id) {
-        return "This is post number " + id + ".";
+    @GetMapping("/posts/show")
+    public String showPost(Model model){
+        Post onePost1 = new Post("A Post", "This is a post");
+        model.addAttribute("onepost", onePost1);
+        return "/posts/show";
     }
 
 
