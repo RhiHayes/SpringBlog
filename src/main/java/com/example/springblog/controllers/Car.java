c38a6aac938043d51781ef6fa827359cae1b3acf
Rhiannon Hayes <rhiannon.nicole.hayes@gmail.com>
2022-01-04 11:46:51 -0600

    Added/Completed Repo + JPA Lecture

---
+++ src/main/java/com/example/springblog/controllers/Car.java
@@ -0,0 +1,36 @@
+package com.example.springblog.controllers;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="cars")
+public class Car {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable= false, length = 50)
+    private String make;
+
+    @Column(nullable= false, length = 50)
+    private String model;
+
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getId() {
+        return id;
+    }
+    public String getMake() {
+        return make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+
+}
